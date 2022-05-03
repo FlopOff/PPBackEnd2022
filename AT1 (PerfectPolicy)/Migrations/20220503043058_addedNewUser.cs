d6c58298d43df25e11566dc490f2e70daa6a3328
FlopOff <99458750+FlopOff@users.noreply.github.com>
2022-05-03 14:42:29 +1000

    Add files via upload

---
+++ AT1 (PerfectPolicy)/Migrations/20220503043058_addedNewUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AT1__PerfectPolicy_.Migrations
+{
+    public partial class addedNewUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 3, 14, 30, 58, 118, DateTimeKind.Local).AddTicks(7763));
+
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 3, 14, 30, 58, 119, DateTimeKind.Local).AddTicks(4892));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserInfoID", "Password", "Username" },
+                values: new object[] { 2, "secret12!", "ben" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserInfoID",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 3, 14, 19, 27, 37, DateTimeKind.Local).AddTicks(4483));
+
+            migrationBuilder.UpdateData(
+                table: "Quizs",
+                keyColumn: "QuizID",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2022, 5, 3, 14, 19, 27, 38, DateTimeKind.Local).AddTicks(2337));
+        }
+    }
+}
---
+++ AT1 (PerfectPolicy)/Models/QuizContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AT1__PerfectPolicy_.Models
+{
+    public class QuizContext : DbContext
+    { 
+   public QuizContext(DbContextOptions options) : base(options)
+    {            
+    }
+
+    public DbSet<Quiz> Quizs { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Option> Options { get; set; }
+    public DbSet<UserInfo> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Quiz>().HasData(
+            new Quiz { QuizID = 1, Title = "QuizTime", Topic = "Quiz", Creator = "Steve", DateCreated = DateTime.Now, PassingPercentage =  12},
+            new Quiz { QuizID = 2, Title = "QuizzyTime", Topic = "Yes", Creator = "Hank", DateCreated = DateTime.Now, PassingPercentage = 60 }
+        );
+
+        builder.Entity<UserInfo>().HasData(
+            new UserInfo { UserInfoID = 1, Username = "Shaun", Password = "abc_1234" },
+            new UserInfo { UserInfoID = 2, Username = "ben", Password = "secret12!" }
+
+            );
+    }
+}
+}
